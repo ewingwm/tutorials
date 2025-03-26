26cfea5d367816c18ed5f20be0f01df81d50fc9d
William Ewing <ewingwm@gmail.com>
2025-03-26 10:16:20 -0400

    Added Estate Tutorial

---
+++ estate/models/estate_property.py
@@ -0,0 +1,27 @@
+from odoo import api
+from odoo import fields
+from odoo import models
+from datetime import date
+from dateutil.relativedelta import relativedelta
+
+class Property(models.Model):
+    _name = "estate.property"
+    _description = "A Property for Real Estate"
+
+    name = fields.Char(required=True)
+    description = fields.Text()
+    postcode = fields.Char()
+    date_avalibility = fields.Date("Date Avalible", copy = False, default=lambda self: date.today() + relativedelta(months=3))
+    expected_price = fields.Float(required=True)
+    selling_price = fields.Float(readonly=True, copy = False)
+    bedrooms = fields.Integer(default = 2)
+    living_area = fields.Integer("Living Area (sqm)")
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    gardern_area = fields.Integer("Garden Area (sqm)")
+    garden_orientation = fields.Selection(selection=[('north','North'),('south','South'),('east','East'),('west','West')])
+    active = fields.Boolean(default=True)
+    state = fields.Selection(selection=[('new','New'),('offer_recived','Offer Recieved'),('offer_accepted','Offer Accepted'),('sold','Sold'),('cancelled','Cancelled')], default = 'new', copy = False, required = True)
+
+
