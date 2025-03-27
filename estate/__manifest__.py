75f3f486836c42ba14f0f2ca631c561986f52105
ewingwm <ewingwm@gmail.com>
2025-03-27 10:12:11 -0400

    Added Property Types Settings Menu

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -3,10 +3,13 @@
     'depends': [
         'base',
     ],
+    'installable': True,
     'application': True,
     'data': [
         'security/ir.model.access.csv',
         'views/estate_property_views.xml',
+        "views/estate_property_types_views.xml",
         'views/estate_menus.xml'
-    ]
+    ],
+    'license': 'LGPL-3',
 }
